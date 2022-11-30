8b61c774e098e570566ab00472519ddcd6950dc5
Leônidas Braga <lebragacool@gmail.com>
2022-11-30 02:24:41 -0300

    tristeza

--- js/calculoMateriais.js
+++ js/calculoMateriais.js
@@ -1,44 +1,96 @@
-export default function calculaMateriais(predio){
+export default function calculaMateriais(predio) {
 
     let total = {
+        "rack_final": {},
+        "par_trancado": 0,
+        "FO": 0,
         "rack": [],//calcular depois
-        "femea": 0,    
+        "femea": 0,
         "espelhos": 0,
         "patch_cords": 0,
         "ppmh": 0,
-        "organizador" : 0,
+        "organizador": 0,
         "patch_cable": 0,
         "bandeja": 0,
         "exaustor": 0,
         "switch": 0,
         "to": 0,
-        "acoplador_longo": 0,
-        "pigtail": 0,
         "pigtail_longo": 0,
-        "cordao_otico": 0,
         "filtro": 0,
+        "dio": 0,
+        "pigtail": 0,
+        "acoplhador": 0,
+        "cordao": 0,
+        "switch_core": 0,
+        "pigtail_principal": 0,
+        "cordao_principal": 0,
+        "cordao_longo": 0,
+        "caixa_emenda": 0,
+        "etq_por_ppmh": 0,
+        "etq_ppmh": 0,
+        "etq_espelho": 0,
+        "etq_utp": 0,
+        "etq_dio": 0,
+        "abracadeira": 0,
+        "porca": 0
     };
 
-    for(let i = 1; i < predio.pontos.length; i++){
+    for (let i = 1; i < predio.pontos.length; i++) {
         let itensAndar = {
-            "rack": 0,//calcular depois
-            "femea": predio.pontos[i]*2,    
-            "espelhos": predio.pontos[i],
-            "patch_cords": predio.pontos[i]*2, 
-            "ppmh": Math.ceil(predio.pontos[i]/12),
-            "organizador" : Math.ceil(predio.pontos[i]/12)*2,
-            "patch_cable": predio.pontos[i]*2,
+            "femea": predio.pontos[i] * 2,
+            "espelhos": Number(predio.pontos[i]),
+            "patch_cords": predio.pontos[i] * 2,
+            "ppmh": Math.ceil(predio.pontos[i] / 12),
+            "organizador": Math.ceil(predio.pontos[i] / 12) * 2,
+            "patch_cable": predio.pontos[i] * 2,
             "bandeja": 1,
             "exaustor": 1,
-            "switch": Math.ceil(predio.pontos[i]/12),
-            "to": i==1?0:Math.ceil(predio.n_fibras/8),
-            "acoplador_longo": i==1?0:Math.ceil(predio.n_fibras/2),
-            "pigtail": i==1?0:predio.n_fibras,
-            "pigtail_longo": i==1?0:predio.n_fibras/2,
-            "cordao_otico": predio.n_fibras/2,
-            "filtro": Math.ceil(predio.pontos[i]/72),
+            "switch": Math.ceil(predio.pontos[i] / 12),
+            "to": i == 1 ? 0 : 1,
+            "pigtail_longo": i == 1 ? 0 : Number(predio.n_fibras),
+            "filtro": Math.ceil(predio.pontos[i] / 72),
+            "dio": 0,
+            "pigtail": 0,
+            "acoplhador": 0,
+            "cordao": 0,
+            "caixa_emenda": 0
         }
-        total.rack.push(itensAndar.rack);
+        if (predio.n_fibras > 8) {
+            itensAndar.to = 0;
+            itensAndar.pigtail_longo = 0;
+            itensAndar.dio = Math.ceil(predio.n_fibras / 24);
+            itensAndar.pigtail = Number(predio.n_fibras);
+            itensAndar.acoplhador = Number(predio.n_fibras);
+            itensAndar.cordao = Number(predio.n_fibras);
+            itensAndar.caixa_emenda = Math.ceil(Number(predio.n_fibras) / 12);
+        }
+
+        let altura_rack = (itensAndar.ppmh + itensAndar.organizador + 4 * itensAndar.bandeja +
+            2 * itensAndar.exaustor + itensAndar.switch + itensAndar.filtro + itensAndar.dio) * 1.2;
+        let tamanhos = [8, 9, 10, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48];
+        let done = false;
+        let rack = 0;
+        let l = 0;
+        while (!done) {
+            if (l == tamanhos.length) {
+                total.rack[48]++;
+                altura_rack = ((altura_rack % 48) + 2 + 4) * 1.2;
+                itensAndar.exaustor++;
+                itensAndar.bandeja++;
+                l = 0;
+            }
+            if (tamanhos[l] >= altura_rack) {
+                rack = tamanhos[l];
+                done = true;
+            }
+            l++;
+        }
+        if(total.rack[rack]==null){
+            total.rack[rack]=1;
+        }
+        else{
+            total.rack[rack]++;
+        }
         total.femea += itensAndar.femea;
         total.espelhos += itensAndar.espelhos;
         total.patch_cords += itensAndar.patch_cords;
@@ -49,14 +101,102 @@ export default function calculaMateriais(predio){
         total.exaustor += itensAndar.exaustor;
         total.switch += itensAndar.switch;
         total.to += itensAndar.to;
-        total.acoplador_longo += itensAndar.acoplador_longo;
-        total.pigtail += itensAndar.pigtail;
         total.pigtail_longo += itensAndar.pigtail_longo;
-        total.cordao_otico += itensAndar.cordao_otico;
         total.filtro += itensAndar.filtro;
+        total.dio += itensAndar.dio;
+        total.pigtail += itensAndar.pigtail;
+        total.acoplhador += itensAndar.acoplhador;
+        total.cordao += itensAndar.cordao;
+        total.caixa_emenda += itensAndar.caixa_emenda;
+
     }
-    
-    predio.total = total;
-    
+    let acumulador = 0;
+    for (let i = 1; i < predio.mh.length; i++) {
+        acumulador += Number(predio.mh[i]) * Number(predio.pontos[i]) * 2;
+    }
+    total.par_trancado = Math.ceil(acumulador / 305);
+
+    if (predio.backbone) {
+        acumulador = 0;
+        let altura = Number(predio.alturas[1]);
+        for (let i = 2; i < predio.alturas.length; i++) {
+            acumulador += altura + Number(predio.alturas[i]) * 2;
+            altura += Number(predio.alturas[i]);
+        }
+        total.FO = acumulador * 1.2;
+
+        let fibrasSEQ = predio.n_fibras * (Number(predio.andares) - 1);
+        total.cordao_longo += Number(predio.n_fibras); //ligacao 1 andar
+        total.dio += Math.ceil(fibrasSEQ / 24);
+        total.acoplhador += fibrasSEQ;
+        total.cordao += fibrasSEQ;
+        total.pigtail += fibrasSEQ;
+        total.caixa_emenda += Math.ceil(fibrasSEQ / 12);
+        total.switch_core += Math.ceil((fibrasSEQ + Number(predio.n_fibras)) / 48);
+        total.filtro += Math.ceil(total.switch_core / 6);
+
+        total.organizador += Math.ceil(fibrasSEQ / 24) +
+            Math.ceil(Number(predio.n_fibras_principal) / 24) + total.switch_core;
+        total.bandeja++;
+        total.exaustor++;
+
+        let altura_rack = (total.switch_core * 2 + Math.ceil(total.switch_core / 6) +
+            2 + 4 + Math.ceil(fibrasSEQ / 24) + total.switch_core +
+            Math.ceil(Number(predio.n_fibras_principal) / 24)) * 1.2;
+        let tamanhos = [8, 9, 10, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48];
+        let done = false;
+        let rack = 0;
+        let k = 0;
+        while (!done) {
+            if (k == tamanhos.length) {
+                total.rack[48]++;
+                altura_rack = ((altura_rack % 48) + 2 + 4) * 1.2;
+                total.exaustor++;
+                total.bandeja++;
+                k = 0;
+            }
+            if (tamanhos[k] >= altura_rack) {
+                rack = tamanhos[k];
+                done = true;
+            }
+            k++;
+        }
+        if(total.rack[rack]==null){
+            total.rack[rack]=1;
+        }
+        else{
+            total.rack[rack]++;
+        }
+        let pregados = total.ppmh + total.organizador + total.switch + total.switch_core +
+            total.dio + total.bandeja;
+        total.porca = Math.ceil(pregados / 10);
+    }
+    if (predio.backbone_sec) {
+        total.pigtail_principal += Number(predio.n_fibras_principal);
+        total.cordao_principal += Number(predio.n_fibras_principal);
+        total.acoplhador += Number(predio.n_fibras_principal);
+        total.dio += Math.ceil(Number(predio.n_fibras_principal) / 24);
+        total.caixa_emenda += Math.ceil(Number(predio.n_fibras_principal) / 12);
+    }
+
+    total.etq_por_ppmh = total.femea;
+    total.etq_espelho = total.espelhos;
+    total.etq_dio = total.acoplhador;
+    total.etq_ppmh = total.ppmh;
+    total.etq_utp = total.femea;
+    total.abracadeira = Math.ceil(total.femea / 100);
+
+    let rackFinal = {};
+    for (let j = 0; j < total.rack.length; j++) {
+        if (total.rack[j] != null && total.rack[j] != 0) {
+            rackFinal[j] = total.rack[j];
+        }
+    }
+    total.rack_final=rackFinal;
+
+    predio.totalAndares = total;
+
+
+
     return predio;
 }
--- js/visualControl.js
+++ js/visualControl.js
@@ -7,13 +7,18 @@ let formFileEl = document.querySelector("#formFile");
 let formContainerEl = document.querySelector("#load");
 let instrucaoEl = document.querySelector("#instrucao");
 let testeBackboneEl = document.querySelector("#teste-backbone");
+let testeBackbone2El = document.querySelector("#teste-backbone2")
 
 let formBackboneSimEl = document.querySelector("#sim");
 let formBackboneSim2El = document.querySelector("#sim2");
+let formBackboneNaoEl = document.querySelector("#nao");
+let formBackboneNao2El = document.querySelector("#nao2");
 let formAndarEl = document.querySelector("#andar");
 let formCatEl = document.querySelector("#cat");
 let formFibraEl = document.querySelector("#fibra");
 let formNFibraEl = document.querySelector("#nfibra");
+let formFibra2El = document.querySelector("#fibra2");
+let formNFibra2El = document.querySelector("#nfibra2");
 
 let predioEl = document.querySelector("#predio");
 let formAlturaEl = document.querySelector("#altura");
@@ -26,7 +31,7 @@ let pagina = 1;
 
 let predioTeste = {
     "itensAndares": [
-        
+
     ],
     "andares": "5",
     "mh": [
@@ -57,20 +62,26 @@ let predioTeste = {
     "backbone": true,
     "backbone_sec": true,
     "fibra": "MM - 50 x 125µm",
-    "n_fibras": "8"
+    "n_fibras": "8",
+    "fibra_principal": "FOSMIG",
+    "n_fibras_principal": "8"
 }
+calcula(predioTeste);
+console.log(predioTeste);
 
 let predio = {
-    total: {},
-    andares : "",
-    mh : [],
-    pontos : [],
-    alturas : [],
-    cat : "",
+    totalAndares: {},
+    andares: "",
+    mh: [],
+    pontos: [],
+    alturas: [],
+    cat: "",
     backbone: false,
     backbone_sec: false,
     fibra: "",
-    n_fibras : ""
+    n_fibras: "",
+    fibra_principal: "",
+    n_fibras_principal: ""
 };
 
 let instrucoes = [
@@ -81,60 +92,85 @@ let instrucoes = [
     "Resultado:"
 ]
 
-function proxPagina(){
-    if(pagina==2){
-        if(formBackboneSimEl.checked == true){
-            if(formAndarEl.value == "" || formCatEl.value == "" || formFibraEl.value == "" || formNFibraEl.value == ""){
+function proxPagina() {
+    if (pagina == 2) {
+        if (formBackboneSim2El.checked == true) {
+            if (formAndarEl.value == "" || formCatEl.value == ""
+                || formFibraEl.value == "" || formNFibraEl.value == ""
+                || formFibra2El.value == "" || formNFibra2El.value == "") {
                 window.alert("Os valores devem ser preenchidos!");
                 return;
             }
+            if (formAndarEl.value == "1") {
+                window.alert("Não é possível ter mais de um backbone em apenas um andar.");
+                return;
+            }
         }
-        else{
-            if(formAndarEl.value == "" || formCatEl.value == ""){
+        if (formBackboneSimEl.checked == true) {
+            if (formAndarEl.value == "" || formCatEl.value == "" || formFibraEl.value == "" || formNFibraEl.value == "") {
                 window.alert("Os valores devem ser preenchidos!");
+                return;
+            }
+            if (formAndarEl.value == "1") {
+                formBackboneSim2El.checked = true;
+                formBackboneSimEl.checked = false;
+                formFibra2El.value = formFibraEl.value;
+                formNFibra2El.value = formNFibraEl.value;
+            }
+        }
+        else {
+            if (formAndarEl.value == "" || formCatEl.value == "") {
+                window.alert("Os valores devem ser preenchidos!");
+                return;
+            }
+            if (formAndarEl.value != "1" && formBackboneSimEl.checked == false) {
+                window.alert("Para mais de um Andar é necessário um backbone.");
                 return;
             }
         }
     }
-    if(pagina==3){
-        if(predio.alturas.length-1 != predio.andares){
+    if (pagina == 3) {
+        if (predio.alturas.length - 1 != predio.andares) {
             window.alert("Configure todos os Andares!");
             return;
         }
     }
-    wrapperEl.style.transform = `translateY(-${100*pagina}vh)`;
+    wrapperEl.style.transform = `translateY(-${100 * pagina}vh)`;
     pagina++;
     instrucaoEl.innerText = instrucoes[pagina];
-    if(pagina==2){
+    if (pagina == 2) {
         nextEl.style.transform = "translateY(0vh)";
     }
-    if(pagina==3){
+    if (pagina == 3) {
         montaPredio();
     }
-    if(pagina==4){
+    if (pagina == 4) {
         nextEl.style.transform = "translateY(190vh)";
         console.log(predio);
         predio = calcula(predio);
+        povoaTabela(predio);
     }
 }
 
-function novoPredio(){
+function novoPredio() {
     formContainerEl.style.display = "none";
     proxPagina();
 }
 
-function revela(){
-    testeBackboneEl.classList.toggle("sumir");
+function revela(e) {
+    e.classList.toggle("sumir");
 }
 
-function montaPredio(){
+function montaPredio() {
     predio.cat = formCatEl.value;
     predio.fibra = formFibraEl.value;
     predio.n_fibras = formNFibraEl.value;
+    predio.fibra_principal = formFibra2El.value;
+    predio.n_fibras_principal = formNFibra2El.value;
     predio.andares = formAndarEl.value;
-    predio.backbone = formBackboneSimEl.checked!=""?true:false;
-    predio.backbone_sec = formBackboneSim2El.checked!=""?true:false;
-    for(let i = 1; i <= predio.andares; i++){
+    predio.backbone = formBackboneSimEl.checked != "" ? true : false;
+    predio.backbone_sec = formBackboneSim2El.checked != "" ? true : false;
+    for (let i = 1; i <= predio.andares; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("andarP")
 
@@ -142,7 +178,7 @@ function montaPredio(){
         let newInput = document.createElement("input");
         newInput.setAttribute("type", "radio");
         newInput.setAttribute("name", "andar");
-        newInput.setAttribute("id",`andar-${i}`);
+        newInput.setAttribute("id", `andar-${i}`);
 
         newLabel.innerText = `Andar ${i}`;
         newLabel.setAttribute("for", `andar-${i}`);
@@ -154,7 +190,7 @@ function montaPredio(){
 
     andares = document.querySelectorAll(".andarP");
     andares.forEach((e) => {
-        e.firstElementChild.addEventListener('change', (el)=>{
+        e.firstElementChild.addEventListener('change', (el) => {
             let idEl = el.target.id;
             id = idEl.split("-")[1];
         })
@@ -163,9 +199,9 @@ function montaPredio(){
 
 let id = 0;
 
-function salvaAndar(){
-    if(id == 0){
-        for(let i = 1; i < andares.length; i++){
+function salvaAndar() {
+    if (id == 0) {
+        for (let i = 1; i < andares.length; i++) {
             predio.mh[i] = formMhEL.value;
             predio.pontos[i] = formPontosEl.value;
             predio.alturas[i] = formAlturaEl.value;
@@ -179,7 +215,111 @@ function salvaAndar(){
     window.alert("Modificado com sucesso!");
 }
 
+let itens = document.querySelectorAll(".resultado");
+let valores = document.querySelectorAll(".alterar");
+let materiaisEl = document.querySelector(".materiais");
+
+function povoaTabela(predioFinal) {
+    valores[0].innerText = `${valores[0].innerText} - ${predioFinal.cat}`;
+    itens[0].innerText = `${predioFinal.totalAndares.par_trancado} caixa(s)`;
+
+    valores[1].innerText = `${valores[1].innerText} - ${predioFinal.cat}`;
+    itens[1].innerText = `${predioFinal.totalAndares.femea} unidade(s)`;
+
+    itens[2].innerText = `${predioFinal.totalAndares.espelhos} unidade(s)`;
+
+    valores[2].innerText = `${valores[2].innerText} - ${predioFinal.cat}`;
+    itens[3].innerText = `${predioFinal.totalAndares.patch_cords} unidade(s)`;
+
+    valores[3].innerText = `${valores[3].innerText} - ${predioFinal.cat}`;
+    itens[4].innerText = `${predioFinal.totalAndares.ppmh} unidade(s)`;
+
+    itens[5].innerText = `${predioFinal.totalAndares.organizador} unidade(s)`;
+
+    valores[4].innerText = `${valores[4].innerText} - ${predioFinal.cat}`;
+    itens[6].innerText = `${predioFinal.totalAndares.ppmh} unidade(s)`;
+
+    itens[7].innerText = `${predioFinal.totalAndares.bandeja} unidade(s)`;
+
+    itens[8].innerText = `${predioFinal.totalAndares.exaustor} unidade(s)`;
+
+    valores[5].innerText = `${valores[5].innerText} - ${predioFinal.cat}`;
+    itens[9].innerText = `${predioFinal.totalAndares.switch} unidade(s)`;
+
+    valores[6].innerText = `${valores[6].innerText} - ${predioFinal.fibra}`;
+    itens[10].innerText = `${predioFinal.totalAndares.FO} m`;
+
+    itens[11].innerText = `${predioFinal.totalAndares.dio} unidade(s)`;
+
+    itens[12].innerText = `${predioFinal.totalAndares.acoplhador} unidade(s)`;
+
+    itens[13].innerText = `${predioFinal.totalAndares.caixa_emenda} unidade(s)`;
+
+    itens[14].innerText = `${predioFinal.totalAndares.to} unidade(s)`;
+
+    valores[7].innerText = `${valores[7].innerText} - ${predioFinal.fibra}`;
+    itens[15].innerText = `${predioFinal.totalAndares.pigtail} unidade(s)`;
+
+    valores[8].innerText = `${valores[8].innerText} - ${predioFinal.fibra_principal}`;
+    itens[16].innerText = `${predioFinal.totalAndares.pigtail_principal} unidade(s)`;
+
+    valores[9].innerText = `${valores[9].innerText} - ${predioFinal.fibra}`;
+    itens[17].innerText = `${predioFinal.totalAndares.pigtail_longo} unidade(s)`;
+
+    valores[10].innerText = `${valores[10].innerText} - ${predioFinal.fibra}`;
+    itens[18].innerText = `${predioFinal.totalAndares.cordao} unidade(s)`;
+
+    valores[11].innerText = `${valores[11].innerText} - ${predioFinal.fibra_principal}`;
+    itens[19].innerText = `${predioFinal.totalAndares.cordao_principal} unidade(s)`;
+
+    valores[12].innerText = `${valores[12].innerText} - ${predioFinal.fibra}`;
+    itens[20].innerText = `${predioFinal.totalAndares.cordao_longo} unidade(s)`;
+
+    itens[21].innerText = `${predioFinal.totalAndares.switch_core} unidade(s)`;
+
+    itens[22].innerText = `${predioFinal.totalAndares.etq_por_ppmh} unidade(s)`;
+
+    itens[23].innerText = `${predioFinal.totalAndares.etq_ppmh} unidade(s)`;
+
+    itens[24].innerText = `${predioFinal.totalAndares.etq_espelho} unidade(s)`;
+
+    itens[25].innerText = `${predioFinal.totalAndares.etq_utp} unidade(s)`;
+
+    itens[26].innerText = `${predioFinal.totalAndares.etq_dio} unidade(s)`;
+
+    itens[27].innerText = `${predioFinal.totalAndares.abracadeira} pacote(s)`;
+
+    itens[28].innerText = `${predioFinal.totalAndares.porca} pacote(s)`;
+
+    itens[29].innerText = `${predioFinal.totalAndares.filtro} unidade(s)`;
+
+    let racks = Object.entries(predioFinal.totalAndares.rack_final);
+    racks.forEach((e) => {
+        let newItem = document.createElement("div");
+        let newSpan1 = document.createElement("span");
+        let newSpan2 = document.createElement("span");
+
+        newItem.appendChild(newSpan1);
+        newItem.appendChild(newSpan2);
+        newSpan2.setAttribute("class", "resultado");
+        newSpan1.innerText = `Rack Fechado 19" - ${e[0]}U`;
+        newSpan2.innerText = `${e[1]} unidade(s)`
+        materiaisEl.appendChild(newItem);
+    })
+
+    itens.forEach(element => {
+        if (element.innerText[0] == "0") {
+            let parent = element.parentNode;
+            let grandparent = parent.parentNode;
+            grandparent.removeChild(parent);
+        }
+    });
+}
+
 botaoAddEl.addEventListener('click', novoPredio);
-formBackboneSimEl.addEventListener('change', revela);
+formBackboneSimEl.addEventListener('change', e => { revela(testeBackboneEl) });
+formBackboneSim2El.addEventListener('change', e => { revela(testeBackbone2El) });
+formBackboneNaoEl.addEventListener('change', e => { revela(testeBackboneEl) });
+formBackboneNao2El.addEventListener('change', e => { revela(testeBackbone2El) });
 nextEl.addEventListener('click', proxPagina);
 botaoSalvarEl.addEventListener("click", salvaAndar);
